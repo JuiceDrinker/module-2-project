1d67148693febb933101d162319d5dd2c52491cf
LolaEnBeta <lolitaenbeta@gmail.com>
2020-02-13 14:37:12 +0100

    Create a router to user profile page

--- cocktail-me/routes/index.js
+++ cocktail-me/routes/index.js
@@ -4,11 +4,17 @@ const router  = express.Router();
 const loginRouter = require("./login");
 const signupRouter = require("./signup");
 
-// Router to /login endpoint
+// Router to login page
 router.use('/login', loginRouter);
 
-//Router to /signup endpoint
+//Router to signup page
 router.use('/signup', signupRouter);
+
+
+//Router to profile page
+router.get('/my-profile', (req, res, next) => {
+  res.render('profile');
+});
 
 /* GET home page */
 router.get('/', (req, res, next) => {
