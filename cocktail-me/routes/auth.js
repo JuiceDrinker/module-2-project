f0c00c8d782e7a0ead09752f82755e3a62f9e70d
LolaEnBeta <lolitaenbeta@gmail.com>
2020-02-13 14:24:49 +0100

    Create router to /signup endpoint

--- cocktail-me/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const authRouter  = express.Router();
-
-/* GET login page */
-authRouter.get('/', (req, res, next) => {
-  res.render('auth/login');
-});
-
-module.exports = authRouter;
--- cocktail-me/routes/index.js
+++ cocktail-me/routes/index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const router  = express.Router();
 
 const loginRouter = require("./login");
+const signupRouter = require("./signup");
 
 // Router to /login endpoint
 router.use('/login', loginRouter);
+
+//Router to /signup endpoint
+router.use('/signup', signupRouter);
 
 /* GET home page */
 router.get('/', (req, res, next) => {
