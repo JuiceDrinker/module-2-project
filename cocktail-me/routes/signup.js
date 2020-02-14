d213cd02f31e5a8fab1c19f3354732b716a7b2f3
JuiceDrinker <subramanianaditya42@gmail.com>
2020-02-14 11:45:54 +0100

    fixed signup bug with terms and services checkbox

--- cocktail-me/routes/signup.js
+++ cocktail-me/routes/signup.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const signupRouter  = express.Router();
+const express = require("express");
+const signupRouter = express.Router();
 
 const User = require("./../models/User");
 
@@ -10,45 +10,53 @@ const saltRounds = 10;
 //Create a user
 signupRouter.post("/", (req, res, next) => {
   const { username, email, password, repeatedPassword, terms, news } = req.body;
-
-  console.log(req.body);
-
-  if (username === "" || password === "" || email === ""){
-    res.render("auth/signup", {messageError: "The username, email and password can't be empty."});
+  if (username === "" || password === "" || email === "") {
+    res.render("auth/signup", {
+      messageError: "The username, email and password can't be empty."
+    });
     return;
   }
 
   if (password !== repeatedPassword) {
-    res.render("auth/signup", {messageError: "The passwords don't match."});
+    res.render("auth/signup", { messageError: "The passwords don't match." });
     return;
   }
 
-  User.findOne({username})
-    .then( (user) => {
+  if (!terms === "on") {
+    res.render("auth/signup", {
+      messageError: "You need to accept our Terms and Services."
+    });
+    return;
+  }
+  User.findOne({ username })
+    .then(user => {
       if (user) {
-        res.render("auth/signup", {messageError: "This username already exists."});
-        console.log(user)
+        res.render("auth/signup", {
+          messageError: "This username already exists."
+        });
         return;
       }
 
       const salt = bcrypt.genSaltSync(saltRounds);
       const hashedPassword = bcrypt.hashSync(password, salt);
 
-      User.create({ username, password: hashedPassword, email })
-        .then( (user) => {
+      User.create({ username, password: hashedPassword, email, terms: true })
+        .then(user => {
           res.redirect("/");
         })
-        .catch( (err) => {
-          res.render("auth/signup", {messageError: "Error while creating the new user."})
+        .catch(err => {
+          res.render("auth/signup", {
+            messageError: "Error while creating the new user."
+          });
           console.log(err);
         });
     })
-    .catch( (err) => console.log(err));
-})
+    .catch(err => console.log(err));
+});
 
 /* GET login page */
-signupRouter.get('/', (req, res, next) => {
-  res.render('auth/signup');
+signupRouter.get("/", (req, res, next) => {
+  res.render("auth/signup");
 });
 
 module.exports = signupRouter;
