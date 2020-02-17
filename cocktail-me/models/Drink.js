de8ef71410905ea03bbb6badd8a8b3cc30a3e76f
JuiceDrinker <subramanianaditya42@gmail.com>
2020-02-17 14:07:50 +0100

    changed Drink model so that it has an array of ingredient names

--- cocktail-me/models/Drink.js
+++ cocktail-me/models/Drink.js
@@ -1,21 +1,28 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const drinkModel = new Schema ({
-  name: {type: String, unique: true, required: true},
-  glass: {type: String},
-  category: {type: String},
-  ingredients: [{
-    unit: {type: String},
-    amount: {type: Number},
-    name: {type: String},
-    ingredientID: {type: Schema.Types.ObjectId, ref: "Ingredient", default: null},
-  }],
-  alcohol: {type: Boolean},
-  garnish: {type: String},
-  preparation: {type: String},
-  private: {type: Boolean, default: false}, // if is true means that the user who created it is the only that can see it
-  userId: {type: Schema.Types.ObjectId, ref: "User", default: null}, // if is null means that is global
+const drinkModel = new Schema({
+  name: { type: String, unique: true, required: true },
+  glass: { type: String },
+  category: { type: String },
+  ingredients: [
+    {
+      unit: { type: String },
+      amount: { type: Number },
+      name: { type: String },
+      ingredientID: {
+        type: Schema.Types.ObjectId,
+        ref: "Ingredient",
+        default: null
+      }
+    }
+  ],
+  ingredientNameArray: [],
+  alcohol: { type: Boolean },
+  garnish: { type: String },
+  preparation: { type: String },
+  private: { type: Boolean, default: false }, // if is true means that the user who created it is the only that can see it
+  userId: { type: Schema.Types.ObjectId, ref: "User", default: null } // if is null means that is global
 });
 
 const Drink = mongoose.model("Drink", drinkModel);
